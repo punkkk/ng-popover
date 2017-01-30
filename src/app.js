b2e8519499604767a5166e76767bddebca3d6cc8
Victor Kozlov <punkrus18@gmail.com>
2017-01-30 17:59:25 +0300

    customize popover; external template; change example; add list to display;

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
 var app = angular.module('testPopover', ['angular-popover'])
 
 app.controller('testCtrl', ['$scope', function($scope) {
-
+    $scope.actionsList = [
+        {
+            action: "action_1",
+            text: "Action 1",
+            class: "simple"
+        }, {
+            action: "action_2",
+            text: "Action 2",
+            class: "simple"
+        }, {
+            action: "action_3",
+            text: "Action 3",
+            class: "other"
+        }
+    ]
 }])
